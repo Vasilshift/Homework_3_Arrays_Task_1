a82d47a47140750029d0bd76248ff4ddc8c73712
Vasilshift <Vasilshift@github.com>
2021-01-03 15:13:28 +0500

    Work version of task. Array = 5 elements

--- Source.cpp
+++ Source.cpp
@@ -1,64 +1,46 @@
 #include <iostream>
 #include <ctime>
+#include <random>
+#include <time.h>
 using namespace std;
 
-int main(int args, char* argv[]) {
+int main() {
 
 	srand(time(NULL));
 
 	const int n = 5;
 
-	int array[n], i;
-	
-
+	int array[n];
+	int i;
 
 	for (i = 0; i < n; i++) {
+
+		array[i] = rand() % n;
+		cout << array[i] << " ";
+	}
 	
-		int x = rand() % n;
+	int max = array[0];
+	int min = array[0];
 
-		cout << x << " ";
+	for (i = 0; i < n; i++) {
 
-		if (array[x] > array[i + 1]) {
-			cout << x << " ";
+		if (array[i] > max) {
 
+			max = array[i];
 		}
 
+		if (array[i] < min) {
+
+			min = array[i];
+		}
 
 	}
-	
-
-
-
-
-
-
-
-
-
 
 	
-	/*
-	for (i = 0; i < 4; i++) {
-		
-		cin >> array[i];
+	cout << " " << endl;
+	cout << "max= " << max << endl;
+	cout << "min= " << min << endl;
 
-	}
-
-	for (i = 0; i < 4; i++) {
-
-		cout << array[i] << " ";
-
-	}
-	*/
-
-
-
-
-
-
-
-
-
-
+	system("pause");
 	return 0;
 }
