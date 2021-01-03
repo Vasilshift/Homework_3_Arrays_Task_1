1b2b0b03177c951e7393f629ccc70e8ade61a298
Vasilshift <Vasilshift@github.com>
2021-01-03 13:47:45 +0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main(int args, char* argv[]) {
+
+	srand(time(NULL));
+
+	const int n = 5;
+
+	int array[n], i;
+	
+
+
+	for (i = 0; i < n; i++) {
+	
+		int x = rand() % n;
+
+		cout << x << " ";
+
+		if (array[i] > array[i + 1]) {
+			cout << x << " ";
+		}
+
+
+	}
+	
+
+
+
+
+
+
+
+
+
+
+	
+	/*
+	for (i = 0; i < 4; i++) {
+		
+		cin >> array[i];
+
+	}
+
+	for (i = 0; i < 4; i++) {
+
+		cout << array[i] << " ";
+
+	}
+	*/
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
